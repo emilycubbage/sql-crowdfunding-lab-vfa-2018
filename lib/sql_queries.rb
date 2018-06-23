56b5fdedf6f5a2f7d8ec72ea7653448411905db3
Emily Cubbage <emilycubbage@gmail.com>
2018-06-23 16:39:43 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,11 +15,11 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, pledges.amount - projects.funding_goal FROM projects INNER JOIN pledges ON projects.id = pledges.project_id WHERE (pledges.amount - projects.funding_goal) >= 0  GROUP BY projects.title; "
+"SELECT projects.title, pledges.amount - projects.funding_goal FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title WHERE (pledges.amount - projects.funding_goal) >= 0; "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amount) FROM users INNER JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY pledges.amount ASC, users.name ASC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
